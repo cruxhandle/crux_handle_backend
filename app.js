85ef791eb833c48463d726b94e745b5982687a5d
cruxhandle <136085862+cruxhandle@users.noreply.github.com>
2023-06-09 17:41:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+dotenv.config()
+
+const send_mail = require("./utils/send_mail")
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.post("/send_mail", async (req, res) => {
+    try {
+        await send_mail(req.body)
+        res.send({ message: "mail sent" })
+    } catch (error) {
+        res.status(400).send({
+            message: "something went wrong"
+        })
+    }
+})
+
+app.use((req, res) => {
+    res.status(404).send({
+        message: "not found"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("server started!")
+})
